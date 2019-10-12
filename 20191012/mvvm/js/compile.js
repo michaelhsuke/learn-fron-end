68fa963a2f91bb8944b9da6c0c730100bad1e9b9
徐可 <xuk21@chinaunicom.cn>
2019-10-12 15:47:12 +0800

    编译模板完成

--- 20191012/mvvm/js/compile.js
+++ 20191012/mvvm/js/compile.js
@@ -10,24 +10,73 @@ class Compile {
 
   run() {
     let fragment = document.createDocumentFragment()
-    let firstChild 
+    let firstChild
     while (firstChild = this.el.firstChild) {
       fragment.appendChild(firstChild)
     }
 
+    this.compile(fragment)
     this.el.appendChild(fragment)
-
-    // debugger
-    // while(firstChild) {
-    //   fragment.appendChild(firstChild)
-    // }
-    // this.compile(fragment)
-    // console.log(typeof this.el)
-    
-    // this.el.append(fragment)
   }
 
   compile(el) {
-    console.log(el)
+    this.walk(el.childNodes)
+  }
+
+  walk(nodes) {
+    if (nodes && nodes.length) {
+      nodes.forEach(item => {
+        this.parseNode(item)
+        if (item.childNodes) {
+          this.walk(item.childNodes)
+        }
+      })
+    }
+  }
+
+  parseNode(node) {
+    if (Utils.isTextNode(node)) {
+      this.parseTextNode(node)
+    } else if (Utils.isElemenent(node)) {
+      this.parseElement(node)
+    }
+  }
+
+  parseTextNode(node) {
+    let textContent = node.textContent
+    if (TEMPLATE_REG.test(textContent)) {
+      CompileUtils.text(node, textContent, this.data)
+    }
+  }
+
+  parseElement(node) {
+    let attrs = node.attributes
+    Object.keys(attrs).forEach(key => {
+      const attr = attrs[key]
+      const attrName = attr.name
+      if (attrName.includes('v-')) {
+        let [ , type] = attrName.split('-')
+        CompileUtils[type](node, attr.value, this.data)
+      }
+    })
+  }
+}
+
+const CompileUtils = {
+  text: function (node, expr, data) {
+    const updateFn = this.updater['text']
+    updateFn && updateFn(node, Utils.matchTplData(expr, data))
+  },
+  model: function (node, expr, data) {
+    const updateFn = this.updater['model']
+    updateFn && updateFn(node, Utils.matchData(expr, data))
+  },
+  updater: {
+    text(node, value) {
+      node.textContent = value
+    },
+    model: function (node, value) {
+      node.value = value
+    }
   }
 }
--- 20191012/mvvm/js/util.js
+++ 20191012/mvvm/js/util.js
@@ -4,5 +4,25 @@ const Utils = {
   },
   isElemenent(node) {
     return node && node.nodeType === 1
+  },
+  isTextNode(node) {
+    return node && node.nodeType === 3
+  },
+  matchData(expr, data) {
+    let result = null
+    if (expr) {
+      result = expr.split('.').reduce((obj, key) => {
+        return obj[key]
+      }, data)
+    }
+    console.log('result=', expr, result)
+    return result
+  },
+  matchTplData(expr, data) {
+    return expr.replace(TEMPLATE_REG, (...args) => {
+      return this.matchData(args[1], data)
+    })
   }
 }
+
+const TEMPLATE_REG = /\{\{\s*(\w+)\s*\}\}/g
