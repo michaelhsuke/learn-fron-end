1ed19fedd83b59987a8b3b25e643f38c956ac865
徐可 <xuk21@chinaunicom.cn>
2019-10-18 18:11:10 +0800

    转换

--- 20191018/async/transform.js
+++ 20191018/async/transform.js
@@ -3,7 +3,14 @@ const fs = require('fs')
 
 var contents = fs.readFileSync('index.js').toString()
 var result = traceur.compile(contents, {
-  filename: 'output.js',
+  filename: 'index.js',
   sourceMap: true,
   modules: 'commonjs'
 })
+
+if (result.error) {
+  throw result.error
+}
+
+fs.writeFileSync('out.js', result.js)
+fs.writeFileSync('out.js.map', result.sourceMap)
