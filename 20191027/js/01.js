dd6e8268e2ea258135e795b2b64aeaf4de7c7fc8
Xu Ke <xuke_m@qq.com>
2019-10-27 23:01:48 +0800

    尝试

--- 20191027/js/01.js
+++ 20191027/js/01.js
@@ -26,14 +26,32 @@ https.get(goodsUrl, res => {
   res.on('end', () => {
     console.log('数据包传输完毕')
     let data = Buffer.concat(chunks, size)
-    console.log(data)
+    // console.log(data)
     let html = data.toString()
     // console.log(html)
     // createFile(html)
-    parseRealUrl(html)
-
+    let res = parseRealUrl(html)
+    if (res.result) {
+      fetchPage(res.url)
+    }
   })
 })
+
+function fetchPage(url) {
+  https.get(url, res => {
+    let chunks = [], size = 0
+    res.on('data', chunk => {
+      chunks.push(chunk)
+      size += chunk.length
+    })
+    res.on('end', () => {
+      console.log('数据包传输完毕')
+      let data = Buffer.concat(chunks, size)
+      let html = data.toString()
+      createFile(html)
+    })
+  })
+}
 
 const createFile = (data) => {
   let filepath = path.resolve(`${__dirname}/${Date.now()}.html`)
@@ -44,10 +62,66 @@ const createFile = (data) => {
   })
 }
 
+// var realUrlExp = /var%s*url%s*=(.+)?/g
+
 const parseRealUrl = (html) => {
-  let res = urlReg.exec(html)
-  console.log(res)
+  let index = html.indexOf('var')
+  while(html && index > -1) {
+    html = html.slice(index + 3)
+    const { result, url, data } = isTargetVar(html, 'url')
+    if (result) {
+      return { result, url }
+
+    } else {
+      html = data
+      index = html.indexOf('var')
+    }
+  }
+  return { result: false }
 }
+
+function isOkLetter(char) {
+  return /\w/.test(char)
+}
+
+function isTargetVar(data, varName) {
+  let index = 0, url
+  while(!/\w/.test(data[index])) {
+    index++
+  }
+  data = data.slice(index)
+  // console.log(data)
+  let dataArr = data.split('=')
+  if (dataArr.length) {
+    let oneVarName = dataArr.shift().trim()
+    if (oneVarName == varName) {  
+      let url = findRealUrl(dataArr.join('='))
+      return { result: true, url }
+    } else {
+      
+      data = dataArr.join('=')
+      return { result: false, data}
+    }
+  }
+
+  return { result: false }
+}
+
+function findRealUrl(data) {
+  let index = 0
+  let result = ''
+  while(!isOkLetter(data[index])) {
+    index++
+  }
+
+  console.log('aaaa', data[index])
+  while(/[a-zA-Z0-9\:\&\%\#\?\/\.\=\@\_]/.test(data[index])) {
+    result += data[index++]
+  }
+  return result
+}
+
+
 
 
 
