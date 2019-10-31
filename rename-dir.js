223d881d0c11c4b0b82ed9e47189603483e3dfb6
徐可 <xuk21@chinaunicom.cn>
2019-10-31 10:13:41 +0800

    目录名修改

--- rename-dir.js
+++ rename-dir.js
@@ -1,3 +1,17 @@
 const fs = require('fs')
 const paths = fs.readdirSync(process.cwd())
-console.log(paths)
+// console.log(paths)
+paths.forEach(name => {
+  if (/^\d+$/.test(name)) {
+    // console.log(name)
+
+    const newName = [name.slice(0, 4), name.slice(4, 6), name.slice(6, 8)].join('-')
+    // console.log(newName)
+    try {
+      fs.renameSync(name, newName)
+    } catch (e) {
+      console.log(e)
+    }
+    
+  }
+})
