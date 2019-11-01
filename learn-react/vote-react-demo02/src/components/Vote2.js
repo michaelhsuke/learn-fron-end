8c994ec112368b5663de59e35c70349fd4a895b7
徐可 <xuk21@chinaunicom.cn>
2019-11-01 09:14:50 +0800

    react16.4前后的生命周期对比

---
+++ learn-react/vote-react-demo02/src/components/Vote2.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default class Vote extends React.Component {
+  constructor(props) {
+    console.log('constructor')
+    super(props)
+    this.state = {
+      m: 0,
+      n: 0
+    }
+  }
+
+  // componentWillMount(...args) { 
+  //   console.log('componentWillMount')
+  //   // console.log(args, this)
+  // }
+
+  static getDerivedStateFromProps() {
+    console.log('getDerivedStateFromProps')
+    return {}
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log('getSnapshotBeforeUpdate')
+    return {}
+  }
+
+  componentDidMount(...args) {
+    console.log('componentDidMount')
+  }
+
+  // componentWillReceiveProps() {
+  //   console.log('componentWillReceiveProps')
+  // }
+
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate')
+    return true
+  }
+
+  // componentWillUpdate() {
+  //   console.log('componentWillUpdate')
+  // }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+  }
+
+  support = () => {
+    const { m } = this.state
+    this.setState({
+      m: m + 1
+    })
+  }
+
+  unsupport = () => {
+    const { n } = this.state
+    this.setState({
+      n: n + 1
+    })
+  }
+
+  render() {
+    console.log('render')
+    const { m, n } = this.state
+    const total = m + n
+    return (
+      <section>
+        <div className="panel panel-default" style={{ margin: "20px auto"}}>
+          <div className="panel-heading">{ this.props.title }</div>
+          <div className="panel-body">
+            <div className="panel__line">支持：{ m }</div>
+            <div className="panel__line">反对：{ n }</div>
+            <div className="panel__line">支持率：{ total === 0 ? '0%': (m / total * 100).toFixed(2) + '%' }</div>
+          </div>
+          <div className="panel-footer">
+            <button className="btn btn-success" style={{ marginRight: '10px'}} onClick={ this.support }>支持</button>
+            <button className="btn btn-danger" onClick={ this.unsupport }>反对</button>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
