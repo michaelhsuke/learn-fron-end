1fe8c82900501d685ee32fdf7f54625b1c40a862
徐可 <xuk21@chinaunicom.cn>
2020-04-08 14:51:34 +0800

    daily

--- review-zhufeng1/2020-04-08/01.js
+++ review-zhufeng1/2020-04-08/01.js
@@ -1,5 +1,7 @@
-function test(...args) {
-  console.log(args)
-}
-
-test()
+(function (exports, require, module, __filename, __dirname) {
+  console.log('exports:', exports)
+  console.log('require:', require)
+  console.log('module:', module)
+  console.log('__filename:', __filename)
+  console.log('__dirname:', __dirname)
+})(exports, require, module, __filename, __dirname)
