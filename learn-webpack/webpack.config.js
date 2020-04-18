11dbaa15f3cc18d67221ba92526bd15a8b044d54
徐可 <xuk21@chinaunicom.cn>
2020-04-18 20:39:36 +0800

    daily

--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   // mode: "production",
@@ -7,5 +9,33 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  } 
+  },
+  devServer: {
+    open: true,
+    port: 8888
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new CleanWebpackPlugin()
+  ]
 }
