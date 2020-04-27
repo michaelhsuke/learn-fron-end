94dc3ff56db5546fe412e7c8d755aa472f4bf668
徐可 <xuk21@chinaunicom.cn>
2020-04-27 08:40:29 +0800

    深度遍历完善

--- 2020-04-27/01.js
+++ 2020-04-27/01.js
@@ -3,14 +3,30 @@ var obj = {
   products: ['apple', 'banana', 'orange'],
   team: {
     developers: ['zhangsan', 'lisi']
+  },
+  time: new Date(),
+  run: function () {
+    console.log('hello')
   }
 }
 
+var a = new Date()
+console.log(typeof a)
+
 var clonedObj = deepClone(obj)
-console.log(clonedObj)
+console.log(obj, clonedObj)
 console.log(clonedObj.products === obj.products)
+console.log(clonedObj.run === obj.run)
 
 function deepClone(obj) {
+  if (obj instanceof Date) {
+    return new Date(obj)
+  }
+
+  // if (obj instanceof Function) {
+  //   return new Function(obj)
+  // }
+
   if (typeof obj != 'object') {
     return obj
   }
