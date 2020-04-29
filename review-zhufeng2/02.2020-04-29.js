d48af8309340095cad5b0044fabe08dca2bd6b57
徐可 <xuk21@chinaunicom.cn>
2020-04-29 16:37:58 +0800

    daily

--- review-zhufeng2/02.2020-04-29.js
+++ review-zhufeng2/02.2020-04-29.js
@@ -8,3 +8,13 @@ console.log(Number([]))
 console.log(Number(![]))
 console.log([] == true)
 console.log(![] == true)
+
+function renderAuthorize(Authorized) {
+  return function (currentAuthority) {
+    if (currentAuthority) {
+      if (typeof currentAuthority === 'function') {
+      
+      }
+    }
+  }
+}
