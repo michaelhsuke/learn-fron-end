51a0da23cf8a34f2b0ae71b723e7a3367de4e5d5
徐可 <xuk21@chinaunicom.cn>
2020-05-09 18:15:35 +0800

    daily

---
+++ review-zhufeng2/44.2020-05-09.js
@@ -0,0 +1,15 @@
+// const person = {
+//   name: 'Lydia',
+//   age: 21
+// }
+
+// let city = person.city
+// city = 'Amesterdam'
+// console.log(person)
+
+const person = {
+  name: 'Lydia',
+  age: 21
+}
+
+// const changeAge = (x = {...})
