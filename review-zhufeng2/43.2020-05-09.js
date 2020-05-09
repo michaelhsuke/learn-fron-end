9bae699a2f10302b572ff80dae1654c2db106ed3
徐可 <xuk21@chinaunicom.cn>
2020-05-09 17:45:26 +0800

    daily

---
+++ review-zhufeng2/43.2020-05-09.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+var data = ["https://m.jf.10010.com/cms/jf-res/mobile/images/fourth/person_item_01.png", "https://m.jf.10010.com/cms/jf-res/mobile/images/fourth/person_item_02.png", "https://m.jf.10010.com/cms/jf-res/mobile/images/fourth/person_item_03.png", "https://m.jf.10010.com/cms/jf-res/mobile/images/fourth/person_item_04.png", "https://m.jf.10010.com/cms/jf-res/mobile/images/fourth/person_item_05.png", "https://m.jf.10010.com/cms/jf-res/mobile/images/fourth/person_item_06.png", "https://m.jf.10010.com/cms/jf-res/mobile/images/fourth/person_item_07.png", "https://m.jf.10010.com/cms/jf-res/mobile/images/fourth/person_item_08.png", "https://m.jf.10010.com/cms/jf-res/mobile/images/fourth/person_item_09.png"]
+
+function getFileName(filepath) {
+  var arr = filepath.split('/')
+  return arr[arr.length - 1]
+}
+
+var promiseArr = []
+
+async function handle() {
+  for (var item of data) {
+    var filename = getFileName(item)
+    promiseArr.push(fetch(item))
+    await fetch(item).then(res => {
+      const dest = fs.createWriteStream(`./file/${filename}`)
+      res.body.pipe(dest)
+    })
+  }
+}
+
+handle()
+
