172b26a95552472726d0bd0053ba6c918a3a57dc
徐可 <xuk21@chinaunicom.cn>
2020-05-20 17:50:40 +0800

    daily

--- learn-webpack-02/src/test.ts
+++ learn-webpack-02/src/test.ts
@@ -5,3 +5,16 @@ interface User {
 
 const user: User = { name: "Hayes", id: 0 };
 console.log(user);
+
+interface IterationResult {
+  value: any;
+  done: boolean;
+}
+
+interface Iterator2 {
+  next(value?: any): IterationResult;
+}
+
+interface Iterable2 {
+  [Symbol.iterator](): Iterator2;
+}
