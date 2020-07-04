b56be80fb4af38d12c9dbe53d7686807e19ce247
徐可 <xuk21@chinaunicom.cn>
2020-07-04 15:58:40 +0800

    翻译脚本

--- ecshop-tools/translate.js
+++ ecshop-tools/translate.js
@@ -1,47 +1,41 @@
-// const fs = require('fs')
-// // console.log(fs)
-// const input = fs.createReadStream('file.txt')
-// // console.log(input)
-// let remainings = ''
-// input.on('data', data => {
-//   // console.log(data)
-
-// })
 const PYTool = require('../lib/ChinesePY')
 const readline = require('readline')
 const fs = require('fs')
+const inFile = './files/file.txt'
+const outFile = './output/mobile.php'
+let result = []  // 处理结果
 
 const r1 = readline.createInterface({
-  input: fs.createReadStream('file.txt'),
+  input: fs.createReadStream(inFile),
 })
 
-
-
-let i = 0
-let result = []  // 保存结果
-// 读取事件
-r1.on('line', line => {
-  let [key, value] = line.split('|')
+// 处理一行
+function handleOneLine(line) {
+  let [ key, value ] = line.split('|')
   key = key.trim()  // 键
   value = value.trim()  // 值
   const jp = PYTool.GetJP(key)
-  result.push(`$_LANG['${jp}'] = "${value}";`)
-})
+  result.push(`$_LANG['e2door_${jp}'] = "${value}";`)
+}
 
-// 关闭事件
-r1.on('close', () => {
-  console.log(result.length)
-
+// 处理结果
+function handleResult() {
   let content = `
     <?php
       ${result.join('\r\n')}
   `
-  fs.writeFile('./mobile.php', content, (err, data) => {
+  fs.writeFile(outFile, content, (err, data) => {
     if (err) {
       throw err
     }
   })
-})
+}
+
+// 读取事件
+r1.on('line', handleOneLine)
+
+// 关闭事件
+r1.on('close', handleResult)
 
 
 
